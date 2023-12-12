f7a500280d007d0a750bf6f99077ceeb11b3cd24
Maria beatriz Lacerda de oliveira <beatrizlacerda2011@gmail.com>
2023-12-12 11:39:48 -0300

    commit02_12_12_2023

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,10 @@ public class ChessMatch {
 
     private void initialSetup(){
         board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
-        board.placePiece(new King(board, Color.WHITE), new Position(0,3));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(0,7));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(7,0));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(7,7));
+        board.placePiece(new King(board, Color.WHITE), new Position(0,4));
+        board.placePiece(new King(board, Color.BLACK), new Position(7,4));
     }
 }
